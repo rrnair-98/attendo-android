9119330b9b4c3fd5d29abb7128433f913bdc89e2
Rahul Ahuja <rahul.ahuja@gocomet.in>
2020-04-06 01:31:15 +0530

    Preferences

--- app/src/main/java/com/rohan/attendo/helpers/TokenHelper.java
+++ app/src/main/java/com/rohan/attendo/helpers/TokenHelper.java
@@ -1,6 +1,8 @@
 package com.rohan.attendo.helpers;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.rohan.attendo.api.models.response.AccessToken;
 
@@ -15,6 +17,8 @@ import java.io.ObjectOutputStream;
  * It is the responsibility of the caller to call the persistToken method to store it.
  */
 public class TokenHelper {
+    private static final String SHARED_PREF_KEY = "ACCESS_TOKEN";
+
     private AccessToken token;
     private Context mContext;
     private static TokenHelper mRef;
@@ -33,27 +37,34 @@ public class TokenHelper {
 
     public void persistToken(AccessToken token){
         this.token = token;
-        try {
-            FileOutputStream fos = this.mContext.openFileOutput("token", Context.MODE_PRIVATE);
-            ObjectOutputStream out = new ObjectOutputStream(fos);
-            out.writeObject(token);
-            fos.close();
-            out.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        SharedPreferences.Editor editor = mContext.getSharedPreferences(SHARED_PREF_KEY, Context.MODE_PRIVATE).edit();
+        String keys[] = { "id", "userId", "accessTokenExpiry", "role", "refreshTokenExpiresAt", "accessToken", "refreshToken"};
+        for(int i = 0;i<keys.length; i++)
+            editor.remove(keys[i]);
+        Log.d("RAHUL", "ROLE = "+token.getRole());
+        editor.putLong("id", token.getId());
+        editor.putLong("userId", token.getUserId());
+        editor.putLong("accessTokenExpiry", token.getCreatedAt());
+        editor.putInt("role", token.getRole());
+        editor.putLong("refreshTokenExpiresAt", token.getRefreshTokenExpiresAt());
+        editor.putString("accessToken", token.getAccessToken());
+        editor.putString("refreshToken", token.getRefreshToken());
+        editor.apply();
     }
 
     public void readToken(){
-        try{
-            FileInputStream fin = this.mContext.openFileInput("token");
-            ObjectInputStream oin = new ObjectInputStream(fin);
-            this.token = (AccessToken) oin.readObject();
-            fin.close();
-            oin.close();
-        }catch (IOException|ClassNotFoundException e){
-            this.token = null;
-            e.printStackTrace();
+        SharedPreferences sharedPref = mContext.getSharedPreferences(SHARED_PREF_KEY, Context.MODE_PRIVATE);
+        token = new AccessToken();
+        token.setId(sharedPref.getLong("id", -100));
+        token.setUserId(sharedPref.getLong("userId", 0));
+        token.setRole(sharedPref.getInt("role", -1000));
+        token.setCreatedAt(sharedPref.getLong("accessTokenExpiry", 0));
+        token.setRefreshTokenExpiresAt(sharedPref.getLong("refreshTokenExpiresAt", 0));
+        token.setAccessToken(sharedPref.getString("accessToken", null));
+        token.setRefreshToken(sharedPref.getString("refreshToken", ""));
+        if (token.getAccessToken() == null){
+            token = null;
+            return;
         }
     }
 
